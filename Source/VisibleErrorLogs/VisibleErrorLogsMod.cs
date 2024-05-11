b070b65d350b3bc84e5a3c5e88ab614b17c24743
Vincent Wong <vincentwwy168@gmail.com>
2024-05-11 21:44:22 +0800

    Initial feature commit

---
+++ Source/VisibleErrorLogs/VisibleErrorLogsMod.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace VisibleErrorLogs
+{
+    public class VisibleErrorLogsMod : Mod
+    {
+        public VisibleErrorLogsMod(ModContentPack content) : base(content)
+        {
+            // since we are dealing with very low-level stuff, we cannot use HugsLib
+            // we therefore need to handle the Harmony init here.
+        }
+    }
+}
