3e9d45a5f3a5d033a12b3ceb8265f2aff4fd91b2
Vincent Wong <vincentwwy168@gmail.com>
2024-05-11 23:34:23 +0800

    Init the Harmony instance manually

--- Source/VisibleErrorLogs/VisibleErrorLogsMod.cs
+++ Source/VisibleErrorLogs/VisibleErrorLogsMod.cs
@@ -1,4 +1,5 @@
-﻿using Verse;
+﻿using HarmonyLib;
+using Verse;
 
 namespace VisibleErrorLogs
 {
@@ -8,6 +9,26 @@ namespace VisibleErrorLogs
         {
             // since we are dealing with very low-level stuff, we cannot use HugsLib
             // we therefore need to handle the Harmony init here.
+            LogInfo("Visible Error Logs, starting up. Hopefully the patches work.");
+            Harmony harmony = new Harmony("rimworld." + content.PackageId);
+            harmony.PatchAll();
+        }
+
+        public static readonly string MODPREFIX = "V1024-VEL";
+
+        public static void LogError(string message)
+        {
+            Log.Error($"[{MODPREFIX}] {message}");
+        }
+
+        public static void LogWarning(string message)
+        {
+            Log.Warning($"[{MODPREFIX}] {message}");
+        }
+
+        public static void LogInfo(string message)
+        {
+            Log.Message($"[{MODPREFIX}] {message}");
         }
     }
 }
