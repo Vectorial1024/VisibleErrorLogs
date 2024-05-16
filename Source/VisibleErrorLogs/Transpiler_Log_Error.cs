9dee71f15ef3539991ab49ae9805d704075ee6a8
Vincent Wong <vincentwwy168@gmail.com>
2024-05-16 22:14:05 +0800

    Adjust patch steps

--- Source/VisibleErrorLogs/Transpiler_Log_Error.cs
+++ Source/VisibleErrorLogs/Transpiler_Log_Error.cs
@@ -16,11 +16,14 @@ namespace VisibleErrorLogs
              * Patch the method, so that the conditional that wraps TryOpenLogWindow() is removed
              * 
              * There is only 1 occurence of TryOpenLogWindow(), and also only 1 occurence of the related checking
+             * 
+             * This related checking occurs right after a function call, which is also occuring only 1 time in the entire function
              */
             return new CodeMatcher(instructions)
                 .MatchStartForward(
-                    new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(PlayDataLoader), nameof(PlayDataLoader.Loaded)))
+                    new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(Log), "PostMessage"))
                 ) // in a cursed approach, find the conditional, and NOP it, thus unwrapping the inner side
+                .Advance(1)
                 // then set NOP 4 times
                 .SetAndAdvance(OpCodes.Nop, null)
                 .SetAndAdvance(OpCodes.Nop, null)
